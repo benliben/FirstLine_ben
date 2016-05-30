4d3b2585632d53b0b268cb1698d6e1cd96c79679
junjie <1287550045@qq.com>
2016-05-30 09:51:19 +0800

    删除了baidu的一个文件

---
+++ app/src/main/java/com/example/benben/firstline/APP.java
@@ -0,0 +1,10 @@
+package com.example.benben.firstline;
+
+import com.example.benben.firstline.ui.activity.BaseActivity;
+
+/**
+ * Created by beneben on 2016/5/30.
+ */
+public class APP extends BaseActivity {
+
+}
